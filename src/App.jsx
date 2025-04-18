18b0e641388bfe78bb88534ab22954391e768b7a
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-04-17 23:03:52 -0300

    finished Hero section -> starting About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from "react";
-import Hero from "./components/Hero";
+import React from "react"
+import Hero from "./components/Hero"
 
 const App = () => {
-  return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Hero />
-    </main>
-  );
-};
+    return (
+        <main className="relative min-h-screen w-screen overflow-x-hidden">
+            <Hero />
+            <section className="z-0 min-h-screen bg-blue-500"></section>
+        </main>
+    )
+}
 
-export default App;
+export default App
